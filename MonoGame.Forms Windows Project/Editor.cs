860939a0935e5823d31ae7d7f4ecc806fc01a50c
Marcel Härtel <m.haertel@gmx.net>
2019-07-06 23:37:43 +0200

    Added the windows project template.

---
+++ MonoGame.Forms Windows Project/Editor.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MonoGame.Forms_Windows_Project
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+    }
+}
