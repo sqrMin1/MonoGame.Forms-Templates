e74a8e1662a45b552c34ef65cc0b11fe24448d17
Marcel Härtel <m.haertel@gmx.net>
2019-07-07 00:13:29 +0200

    Additional template info and content.

--- MonoGame.Forms Windows Project/Properties/AssemblyInfo.cs
+++ MonoGame.Forms Windows Project/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("MonoGame.Forms Windows Project")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A MonoGame.Forms Editor project for the Windows desktop using DirectX.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MonoGame.Forms Windows Project")]
